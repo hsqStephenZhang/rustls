b527af8b43373cbb5aa29ed69127ebcedd5840c4
hsqStephenZhang <stephenzhang666666@gmail.com>
2025-01-25 15:30:56 +0000

    fix: fix fingerprint demo

--- examples/src/bin/fingerprint.rs
+++ examples/src/bin/fingerprint.rs
@@ -1,6 +1,21 @@
 //! the ja4 fingerprint should be:
-//! chrome108: t13d1516h1_8daaf6152771_5fb3489db586
-//! firefox: t13d1516h1_8daaf6152771_5fb3489db586
+//! first: proto, tlsVersion, sniMode, numSuites, numExtensions, firstALPN
+//! second: sha256(suites)
+//! third: sha256(extensions)
+
+/// results of us
+// chrome108: t13d1516h1_8daaf6152771_5fb3489db586
+// edge106: t13d1616h1_e72c3b3287f1_5fb3489db586
+// firefox105: t13d1715h1_5b57614c22b0_5a7a167d0339
+// safari17: t13d2014h1_a09f3c656075_f62623592221
+// ios14: t13d2613h1_9f5fa85aebfd_fda70f8e401f
+
+// results of utls: code(https://gist.github.com/hsqStephenZhang/464792650322426c619ace61bdbc5ff5)
+// chrome115: t13d1515h2_8daaf6152771_e5627efa2ab1
+// edge106: t13d1616h2_e72c3b3287f1_5fb3489db586
+// firefox105: t13d1715h2_5b57614c22b0_5a7a167d0339
+// safari16: t13d2014h2_a09f3c656075_f62623592221
+
 use std::io::{stdout, Read, Write};
 use std::net::TcpStream;
 use std::sync::Arc;
@@ -13,6 +28,7 @@ enum Fingerprint {
     Chrome108,
     Firefox,
     Safari,
+    Edge106,
 }
 
 impl From<&str> for Fingerprint {
@@ -21,6 +37,7 @@ impl From<&str> for Fingerprint {
             "chrome108" => Fingerprint::Chrome108,
             "firefox" => Fingerprint::Firefox,
             "safari" => Fingerprint::Safari,
+            "edge106" => Fingerprint::Edge106,
             _ => panic!("Invalid fingerprint"),
         }
     }
@@ -55,6 +72,9 @@ fn main() {
         Fingerprint::Safari => rustls::craft::SAFARI_17_1
             .get(opts.alpn)
             .builder(),
+        Fingerprint::Edge106 => rustls::craft::EDGE_106
+            .get(opts.alpn)
+            .builder(),
     };
 
     let mut config = rustls::ClientConfig::builder()
@@ -71,16 +91,6 @@ fn main() {
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
     tls.write_all(concat!("GET /api/all HTTP/1.1\r\n", "\r\n").as_bytes())
         .unwrap();
-    let ciphersuite = tls
-        .conn
-        .negotiated_cipher_suite()
-        .unwrap();
-    writeln!(
-        &mut std::io::stderr(),
-        "Current ciphersuite: {:?}",
-        ciphersuite.suite()
-    )
-    .unwrap();
     let mut plaintext = Vec::new();
     tls.read_to_end(&mut plaintext).unwrap();
     stdout().write_all(&plaintext).unwrap();
